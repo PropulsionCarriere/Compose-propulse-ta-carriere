b546cc68d3060ece236d27e3db81b34454de5379
Mick00 <unconfigured@null.spigotmc.org>
2020-08-11 12:07:04 -0400

    add images to pull

--- upgrade.sh
+++ upgrade.sh
@@ -2,6 +2,8 @@
 
 sh export.sh
 mkdir -p logs
+docker pull docker.pkg.github.com/propulsioncarriere/propulsetacarriere/app:latest
+docker pull docker.pkg.github.com/propulsioncarriere/propulsetacarriere/nginx:latest
 docker-compose build --pull
 docker-compose up -d --force-recreate
 docker-compose exec app php artisan migrate
